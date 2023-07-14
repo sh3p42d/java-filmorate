6bd3bf4ec8ffe2d982b01644b61d396cb4339fa1
Ruslan Abashin <abashin.r.s@yandex.ru>
2023-07-14 14:09:24 +0700

    fix: удалил абстрактный контроллер

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,25 +1,67 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import ru.yandex.practicum.filmorate.exceptions.NotPresentException;
 import ru.yandex.practicum.filmorate.model.Film;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.ValidationException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
 @RequestMapping("/films")
-public class FilmController extends RequestController<Film> {
+public class FilmController {
+    protected int id = 1;
+    protected final Map<Integer, Film> mapOfInfo = new HashMap<>();
 
-    @Override
+    @GetMapping
+    public List<Film> findAll() {
+        return new ArrayList<>(mapOfInfo.values());
+    }
+
+    @PostMapping
+    public Film create(@Valid @RequestBody Film film) {
+        try {
+            addId(film, id);
+            mapOfInfo.put(id, film);
+            log.info("Добавлен {}: {}", film.getClass(), film);
+            id++;
+        } catch (ValidationException e) {
+            log.error("Ошибка в теле запроса");
+        }
+        return film;
+    }
+
+    @PutMapping
+    public Film update(@Valid @RequestBody Film film) {
+        try {
+            if (checkIsExist(film)) {
+                mapOfInfo.put(extractId(film), film);
+                log.info("Обновлен {}: {}", film.getClass(), film);
+            } else {
+                log.info("{} для обновления не найден", film);
+                throw new NotPresentException(HttpStatus.NOT_FOUND, "Нет такого " + film);
+            }
+        } catch (ValidationException e) {
+            log.error("Ошибка в теле запроса");
+        }
+        return film;
+    }
+
     protected boolean checkIsExist(Film film) {
         return mapOfInfo.containsKey(film.getId());
     }
 
-    @Override
     protected int extractId(Film film) {
         return film.getId();
     }
 
-    @Override
     protected void addId(Film film, int id) {
         film.setId(id);
     }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,25 +1,67 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import ru.yandex.practicum.filmorate.exceptions.NotPresentException;
 import ru.yandex.practicum.filmorate.model.User;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.ValidationException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
 @RequestMapping("/users")
-public class UserController extends RequestController<User> {
+public class UserController {
+    protected int id = 1;
+    protected final Map<Integer, User> mapOfInfo = new HashMap<>();
 
-    @Override
+    @GetMapping
+    public List<User> findAll() {
+        return new ArrayList<>(mapOfInfo.values());
+    }
+
+    @PostMapping
+    public User create(@Valid @RequestBody User user) {
+        try {
+            addId(user, id);
+            mapOfInfo.put(id, user);
+            log.info("Добавлен {}: {}", user.getClass(), user);
+            id++;
+        } catch (ValidationException e) {
+            log.error("Ошибка в теле запроса");
+        }
+        return user;
+    }
+
+    @PutMapping
+    public User update(@Valid @RequestBody User user) {
+        try {
+            if (checkIsExist(user)) {
+                mapOfInfo.put(extractId(user), user);
+                log.info("Обновлен {}: {}", user.getClass(), user);
+            } else {
+                log.info("{} для обновления не найден", user);
+                throw new NotPresentException(HttpStatus.NOT_FOUND, "Нет такого " + user);
+            }
+        } catch (ValidationException e) {
+            log.error("Ошибка в теле запроса");
+        }
+        return user;
+    }
+
     protected boolean checkIsExist(User user) {
         return mapOfInfo.containsKey(user.getId());
     }
 
-    @Override
     protected int extractId(User user) {
         return user.getId();
     }
 
-    @Override
     protected void addId(User user, int id) {
         user.setId(id);
     }
