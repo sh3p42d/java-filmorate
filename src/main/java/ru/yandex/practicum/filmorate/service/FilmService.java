76a318b2c8daa874f1ebdbafc165acdfc1496f57
Ruslan Abashin <abashin.r.s@yandex.ru>
2023-07-14 22:52:36 +0700

    fix: поправил кодстайл

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.service;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exceptions.NotPresentException;
 import ru.yandex.practicum.filmorate.model.Film;
