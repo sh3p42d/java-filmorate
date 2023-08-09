dbc33cd164962fe352e8e1db85ade88e2408f40c
Ruslan Abashin <abashin.r.s@yandex.ru>
2023-08-10 00:41:20 +0700

    fix: правки кодстайла

--- src/test/java/ru/yandex/practicum/filmorate/controller/ControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/ControllerTest.java
@@ -16,7 +16,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.test.web.servlet.MvcResult;
